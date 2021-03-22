547837d7cb8676ee4433498b7c796b5030580d2c
Dzianis Khatsuk <dennis.hatsuk@gmail.com>
2021-03-22 23:27:06 +0300

    feat: use useEffect to load data from server

--- src/index.js
+++ src/index.js
@@ -10,9 +10,10 @@ const App = () => {
       <div>
         <button onClick = { () => setValue((value) => value + 1 ) }>+</button>
         <button onClick = { () => setVisible(false) }>Hide</button>
-        <ClassCounter value = { value } />
+        {/* <ClassCounter value = { value } />
         <HookCounter value = { value } />
-        <Notification />
+        <Notification /> */}
+        <PlanetInfo id = { value }/>
       </div>
     );
   } else {
@@ -77,6 +78,25 @@ const HookCounter = ({ value }) => {
   // }, [ value ]);
 
   return <div>{ value }</div>
+};
+
+const PlanetInfo = ({ id }) => {
+
+  const [ planet, setPlanet ] = useState('Wait for planet name load');
+
+  useEffect(() => {
+    let isCancelled = false;
+    fetch(`https://swapi.dev/api/planets/${id}`)
+    .then((res) => res.json())
+    .then((planet) => !isCancelled && setPlanet(planet.name));
+    return () => isCancelled = true;
+  }, [id]);
+
+  return (
+    <div>
+      { id } - { planet }
+    </div>
+  );
 };
 
 ReactDOM.render(<App />,
