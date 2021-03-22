97a136c4f4ba4391eed33d718e3d681cfc7926e5
Dzianis Khatsuk <dennis.hatsuk@gmail.com>
2021-03-22 22:49:48 +0300

    feat: useEffect lecture

--- src/index.js
+++ src/index.js
@@ -1,19 +1,83 @@
-import React, { useContext } from 'react';
+import React, { Component, useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 
-const MyContext = React.createContext();
-
 const App = () => {
-  return (
-    <MyContext.Provider value = 'Hello World!'>
-      <Child />
-    </MyContext.Provider>
-  );
+  const [value, setValue] = useState(0);
+  const [visible, setVisible] = useState(true);
+
+  if (visible) {
+    return (
+      <div>
+        <button onClick = { () => setValue((value) => value + 1 ) }>+</button>
+        <button onClick = { () => setVisible(false) }>Hide</button>
+        <ClassCounter value = { value } />
+        <HookCounter value = { value } />
+        <Notification />
+      </div>
+    );
+  } else {
+    return (
+      <button onClick = { () => setVisible(true) }>Show</button>
+    );
+  }
 };
 
-const Child = () => {
-  const value = useContext(MyContext);
-  return <p>{ value }</p>;
+class ClassCounter extends Component {
+  componentDidMount() {
+    console.log('[Class]: mount');
+  }
+  
+  componentDidUpdate() {
+    console.log('[Class]: update');
+  }
+
+  componentWillUnmount() {
+    console.log('[Class]: unmount');
+  }
+
+  render() {
+    const { value } = this.props;
+    return <div>{ value }</div>
+  }
+
 };
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Notification = () => {
+
+  const [ isVisible, setVisibility ] = useState(true);
+
+  useEffect(() => {
+    console.log('[Notification]: mount')
+    return () => console.log('[Notification]: unmount');
+  }, []);
+
+  useEffect(() => {
+    const notificationTimeout = setTimeout(() => {
+      setVisibility(false);
+    }, 1500);
+    return () => clearTimeout(notificationTimeout);
+  }, []);
+
+  useEffect(() => {
+    console.log('[Notification]: update');
+  });
+  if (isVisible) {
+    return <div><p>Notification</p></div>;
+  } else {
+    return null;
+  }
+};
+
+const HookCounter = ({ value }) => {
+  // useEffect(() => {
+  //   console.log('[func]: useEffect()');
+  //   return () => {
+  //     console.log('clear');
+  //   };
+  // }, [ value ]);
+
+  return <div>{ value }</div>
+};
+
+ReactDOM.render(<App />,
+  document.getElementById('root'));
